59d8f3a9081ea51a654d4f8fa5a377cb21459edd
RafaelLiWu <89156637+RafaelLiWu@users.noreply.github.com>
2021-10-27 11:32:03 -0300

    Add files via upload

--- js/at.js
+++ js/at.js
@@ -1,3 +1,6 @@
 let mudar = (e) => {
-    
+    $('.obras-buttons .list-group-item').removeClass('active');
+    $(e).addClass('active');
+    let key = $(e).attr("data-key");
+    document.querySelector('.obras-sliders-inner').style.marginLeft = `calc(-300px * ${key})`
 }
